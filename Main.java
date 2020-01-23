12bae7f4f3e59b0f2b85d3329bdfc9d4a434278c
howlcipher <wylelias.123@gmail.com>
2020-01-23 02:56:45 +0000

    Created a sudo code for future menu of options
    created simulated user input for creating shoes.

--- Main.java
+++ Main.java
@@ -1,11 +1,45 @@
 
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Let's track your shoes to make sure you are running strong!");
-    Shoes runningShoes = new Shoes("Mizuno", "Waver Runner", 300);
+      boolean newShoes = true;    
+
+    
+    /* 
+    SWITCH CASE MENU
+    1 New shoes
+        create new shoes
+    2 Load Shoes
+        load shoes from file
+    3 Save shoes
+        save current shoes new or loaded
+    4 update shoes
+        update information on shoes
+    5 delete shoes
+        remove a shoe from the saves
+  
+    */
+
+    if (newShoes){ 
+    //ask user to input information
+    String brand = "Mizuno";
+    String model = "Waver Rider";
+    int lifeOfShoe = 300;
+    
+    //new shoes creates
+    Shoes runningShoes = new Shoes(brand, model, lifeOfShoe);
+    
+
     System.out.println("Brand: " + runningShoes.getBrand());
     System.out.println("Model: " + runningShoes.getModel());
     System.out.println("Estimated Miles of Life: " + runningShoes.getlifeOfShoe());
-
+    
+    //-30 should be the user input
+    runningShoes.setLifeOfShoe(runningShoes.getlifeOfShoe()-30);
+    System.out.println("New Estimated Miles of Life: " + runningShoes.getlifeOfShoe());
+    } else {
+      System.out.println("No shoes to be recorded");
+    }
   }
 }
